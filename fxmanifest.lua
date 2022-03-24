6c01efe7687a6c7d7ff1d319fd776b6060ef75d8
Maurice .H <66009198+WilderMO@users.noreply.github.com>
2022-03-24 14:35:22 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Creator >> Thunderstorm441    Now Mentained by >> Wilder MO'
+description 'A Spawn selector for FiveM'
+version '1.1.0'
+
+-- What to run
+client_scripts {
+    'spawnselect_client.lua',
+}
