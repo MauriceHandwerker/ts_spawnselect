2889acf82cc617ecbede6449ce5010680c0dc14a
Maurice .H <66009198+WilderMO@users.noreply.github.com>
2022-06-01 12:08:12 +0200

    Update spawnselect_client.lua

--- spawnselect_client.lua
+++ spawnselect_client.lua
@@ -113,27 +113,3 @@ Citizen.CreateThread(function()
 end)
 
 
----- GitHub Updater ----
-function GetCurrentVersion()
-	return GetResourceMetadata( GetCurrentResourceName(), "version" )
-end
-
-local CurrentVersion = GetCurrentVersion()
-local resourceName = "^4["..GetCurrentResourceName().."]^0"
-
-if Config.VersionChecker then
-	PerformHttpRequest('url', function(Error, NewestVersion, Header)
-		print("###############################")
-    	if CurrentVersion == NewestVersion then
-	    	print(resourceName .. '^2 ✓ Resource is Up to Date^0 - ^5Current Version: ^2' .. CurrentVersion .. '^0')
-    	elseif CurrentVersion ~= NewestVersion then
-        	print(resourceName .. '^1 ✗ Resource Outdated. Please Update!^0 - ^5Current Version: ^1' .. CurrentVersion .. '^0')
-	    	print('^5Newest Version: ^2' .. NewestVersion .. '^0 - ^6Download here: ^9url2'.. NewestVersion .. '^0')
-    	end
-		print("###############################")
-	end)
-else
-	print("###############################")
-	print(resourceName .. '^2 ✓ Resource loaded^0')
-	print("###############################")
-end
