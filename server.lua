0944bd0cc788c596d1f16a68a6b0abadd34f5d8d
Maurice .H <66009198+WilderMO@users.noreply.github.com>
2022-06-01 12:11:11 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -6,14 +6,14 @@ end
 local CurrentVersion = GetCurrentVersion()
 local resourceName = "^4["..GetCurrentResourceName().."]^0"
 
-if Config.VersionChecker then
+if Config.EnableVersionControl then
 	PerformHttpRequest('https://github.com/WilderMO/ts_spawnselect/blob/master/VERSION', function(Error, NewestVersion, Header)
 		print("###############################")
     	if CurrentVersion == NewestVersion then
 	    	print(resourceName .. '^2 ✓ Resource is Up to Date^0 - ^5Current Version: ^2' .. CurrentVersion .. '^0')
     	elseif CurrentVersion ~= NewestVersion then
         	print(resourceName .. '^1 ✗ Resource Outdated. Please Update!^0 - ^5Current Version: ^1' .. CurrentVersion .. '^0')
-	    	print('^5Newest Version: ^2' .. NewestVersion .. '^0 - ^6Download here: ^9url2'.. NewestVersion .. '^0')
+	    	print('^5Newest Version: ^2' .. NewestVersion .. '^0 - ^6Download here: ^9https://github.com/WilderMO/ts_spawnselect/releases/tag/v'.. NewestVersion .. '^0')
     	end
 		print("###############################")
 	end)
