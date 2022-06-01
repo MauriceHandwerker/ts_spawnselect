b5a19e9de693f7b90a4e5e25ab93cbf0adb9076d
Maurice .H <66009198+WilderMO@users.noreply.github.com>
2022-06-01 12:12:38 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,6 +7,12 @@ version '1.1.0'
 
 -- What to run
 client_scripts {
+    'config.lua',
     '@NativeUI/NativeUI.lua',
     'spawnselect_client.lua'
+}
+
+server_scripts {
+    'config.lua',
+    'server.lua'
 }
